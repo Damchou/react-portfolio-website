bef4a5ea84b17abbc11d3dec68d01815326e6f15
Marko <m.j.niiranen@gmail.com>
2020-01-20 17:14:39 +0200

    Add hover text when mouse is over Project's icon

--- src/Components/Thumbnail.js
+++ src/Components/Thumbnail.js
@@ -1,30 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import Popup from "reactjs-popup";
 import ProjectItem from "./ProjectItem";
 
-export default function Thumbnail(props) {
-  return <div className="projects-item">{popup(props)}</div>;
-}
+export default class Thumbnail extends Component {
+  constructor(props) {
+    super(props);
 
-function popup(props) {
-  return (
-    <Popup
-      trigger={
-        <div>
-          <img
-            className="projects-item-icon"
-            src={require(`../Images/${props.path}/${props.icon}.png`)}
-          />
-          <div className="projects-item-title">{props.name}</div>
-        </div>
-      }
-      modal
-    >
-      {close => (
-        <div className="popup">
-          <ProjectItem path={props.path} />
-        </div>
-      )}
-    </Popup>
-  );
+    this.state = {
+      opacity: 0
+    };
+  }
+
+  mouseEnter = () => {
+    this.setState({ opacity: 1 });
+  };
+
+  mouseLeave = () => {
+    this.setState({ opacity: 0 });
+  };
+
+  hover() {
+    return (
+      <div
+        className="projects-item-title"
+        style={{ opacity: this.state.opacity }}
+      >
+        {this.props.name}
+      </div>
+    );
+  }
+
+  popup(props) {
+    return (
+      <Popup
+        trigger={
+          <div>
+            <img
+              className="projects-item-icon"
+              src={require(`../Images/${this.props.path}/${this.props.icon}.png`)}
+            />
+            {this.hover()}
+          </div>
+        }
+        modal
+      >
+        {close => (
+          <div className="popup">
+            <ProjectItem path={this.props.path} />
+          </div>
+        )}
+      </Popup>
+    );
+  }
+
+  render() {
+    return (
+      <div
+        className="projects-item"
+        onMouseEnter={this.mouseEnter}
+        onMouseLeave={this.mouseLeave}
+      >
+        {this.popup(this.props)}
+      </div>
+    );
+  }
 }
